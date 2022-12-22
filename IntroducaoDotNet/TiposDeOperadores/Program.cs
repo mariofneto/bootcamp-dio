8eccafe45891257ade4dbc781b326be3867266ce
Mário Ferreira <marioo.netoo3@gmail.com>
2022-12-22 14:06:56 -0300

    estruturas de repetição adicionada exemplo

--- IntroducaoDotNet/TiposDeOperadores/Program.cs
+++ IntroducaoDotNet/TiposDeOperadores/Program.cs
@@ -35,3 +35,73 @@
 //  OR é uma condição ou outra, alguma tem que ser verdadeira
 //  AND é que as duas condições sejam verdadeiras
 //  NOT é o contrário da condição,se ela é true sera false, se for false sera true
+
+// =======================================
+//        Operadores Aritméticos
+//                  &
+//             Classe Math
+// =======================================
+
+// Operadores básicos: +, -, *, /
+
+// Potência com Classe Math: Math.Pow()
+// Para arredondar os digitos: Math.Round(numero, quantos digitos depois da vírgula)
+// 
+// =======================================
+//         Laços de Repetição
+// =======================================
+
+// Tipos de Laços de Repetição: FOR, FOR EACH, WHILE e DO WHILE
+
+// FOR vai ter a declaração, condição e incremento 
+
+// var numeroParaTabuada = 50;
+
+// for(var contador = 1; contador <= 10; contador++)
+//  Console.WriteLine($"{numeroParaTabuada} x {contador} = {numeroParaTabuada * contador}");
+
+
+// WHILE vai ter so a condição entre os parenteses, o restante é no bloco do código
+
+// var numeroParaTabuada = 6;
+// var contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numeroParaTabuada} x {contador} = {numeroParaTabuada * contador}");
+//     contador++;
+// }
+
+// DO WHILE vai ter seu teste lógico no final, enquanto o WHILE é no começo
+// entao mesmo que seja falsa a condição é executada pelo menos uma vez!
+
+// var numeroParaTabuada = 6;
+// var contador = 1;
+
+// do
+// {
+//     Console.WriteLine($"{numeroParaTabuada} x {contador} = {numeroParaTabuada * contador}");
+//     contador++;
+// }
+// while (contador <= 10);
+
+// =======================================
+//         Um Exemplo Legal 
+// =======================================
+
+// Console.WriteLine("||Digite vários números para somar|| (0 para encerrar o programa)");
+// var numeroDigitado = 0;
+// List<int> numerosParaSomar = new List<int>();
+
+// do
+// {
+//     Console.WriteLine("Escreva o número: ");
+//     numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+//     numerosParaSomar.Add(numeroDigitado);
+// }
+// while (numeroDigitado != 0);
+
+// Console.WriteLine($"A soma dos números que você escreveu é igual a {numerosParaSomar.Sum()}");
+
+// Environment.Exit(0) vai sair do programa sem executar nada que tem depois!
