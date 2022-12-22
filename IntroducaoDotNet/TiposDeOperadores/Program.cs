4cffb6097d7f4114e12daf1572448fe607199380
Mário Ferreira <marioo.netoo3@gmail.com>
2022-12-22 08:08:55 -0300

    adicionado Operadores Condicionais

--- IntroducaoDotNet/TiposDeOperadores/Program.cs
+++ IntroducaoDotNet/TiposDeOperadores/Program.cs
@@ -1,4 +1,8 @@
-﻿// int a = 10;
+﻿// =======================================
+//        Operadores de Atribuição
+// =======================================
+
+// int a = 10;
 // int b = 20;
 
 // int c = a + b;
@@ -6,6 +10,18 @@
 // Console.WriteLine(c);
 // Console.WriteLine(c + 5);
 
-var a = Convert.ToInt32("100055");
+// var a = Convert.ToInt32("100055");
 
-Console.Write(a);
+// Console.Write(a);
+
+// =======================================
+//        Operadores Condicionais
+// =======================================
+
+// var quantidadeEstoque = 10;
+// var quantidadeCompra = 4;
+
+// if(quantidadeCompra <= quantidadeEstoque)
+//     Console.WriteLine("Compra feita com sucesso!");
+// else
+//     Console.WriteLine($"Não temos essa quantidade de produto, temos apenas {quantidadeEstoque}");
