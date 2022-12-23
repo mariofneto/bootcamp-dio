48939b3f2970398a4bb9e311732acb8bc8b60fad
Mário Ferreira <marioo.netoo3@gmail.com>
2022-12-23 08:36:40 -0300

    começando a parte de array e listas

---
+++ fundamentos/ArrayListas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// ================================
+//     Sintaxe Básica Array
+// ================================
+
+// int[] array = new int[4];
+// int[] array = new int[] {42, 75, 74, 61};
+// string[] meses = {"Jan","Fev"};
+
+// o ruim do array é ele ter tamanho fixo, por isso prefiro listas
+
+// ================================
+//     Exemplo Array
+// ================================
+
+int[] array = new int[] { 42, 75, 774, 69 };
+
+for (var contador = 0; contador < array.Length; contador++)
+    Console.WriteLine($"O índice {contador} tem o valor {array[contador]}");
+
+
