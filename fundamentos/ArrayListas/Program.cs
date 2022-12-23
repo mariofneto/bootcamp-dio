75971778fd0f3f42c8d8c78159dcf22088f64822
Mário Ferreira <marioo.netoo3@gmail.com>
2022-12-23 09:29:21 -0300

    fim dos arrays e lists

--- fundamentos/ArrayListas/Program.cs
+++ fundamentos/ArrayListas/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-// ================================
+﻿// ================================
 //     Sintaxe Básica Array
 // ================================
 
@@ -10,12 +9,40 @@
 // o ruim do array é ele ter tamanho fixo, por isso prefiro listas
 
 // ================================
-//     Exemplo Array
+//     Exemplo Array com FOR
 // ================================
 
-int[] array = new int[] { 42, 75, 774, 69 };
+// int[] array = new int[] { 42, 75, 774, 69 };
 
-for (var contador = 0; contador < array.Length; contador++)
-    Console.WriteLine($"O índice {contador} tem o valor {array[contador]}");
+// for (var contador = 0; contador < array.Length; contador++)
+//     Console.WriteLine($"O índice {contador} tem o valor {array[contador]}");
+
+// ================================
+// Mesmo Exemplo Array com FOREACH
+// ================================
+
+// var contador = 0;
+// foreach (var valor in array)
+// {
+//     Console.WriteLine($"No índice {contador}, o valor do Array é {valor}");
+//     contador++;
+// }
+
+// =============================================
+//    Sintaxe Básica List (Um array melhorado)
+// =============================================
+
+var pessoa = new List<string>();
+
+pessoa.Add("Mário");
+pessoa.Add("Odila");
+pessoa.Add("Julia");
+
+foreach(var nomePessoa in pessoa)
+    Console.WriteLine(nomePessoa.ToLower().Contains("i")); // saber se cada nome tem a letra "i"
+
+
+
+
 
 
