b6c7b14e8e57a1ab7fb4b7e3e9004390ae44d028
Mário Ferreira <marioo.netoo3@gmail.com>
2023-01-04 09:43:22 -0300

    fim do curso de mvc

--- mvc/Controllers/ContatoController.cs
+++ mvc/Controllers/ContatoController.cs
@@ -59,5 +59,33 @@ namespace mvc.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+            return View(contato);
+        }
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            _context.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
